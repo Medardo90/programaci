e47ac24e899a6f92388c0f73ab1b268b8e5deafa
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2022-05-10 08:52:36 -0500

    Add files via upload

---
+++ untitled6.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:39:52 2022
+
+@author: Alumno
+"""
+
+contar=input("ingrese el # al que debo contar: ")
+contar=int(contar)
+contador=1
+while True:
+    print(contador)
+contador<=contar:
+    print(contador)
+    contador+=1
+    if contador>contar:
+        break
